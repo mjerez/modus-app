923950375c6cae3bb4af00de796d8ed1cb428ccf
Maria Teresa Novillo <mtn2085@MacBook-Air-de-Maria.local>
2018-06-16 23:16:14 -0300

    adding tables and data

--- db/create_db.sql
+++ db/create_db.sql
@@ -1,1 +1,28 @@
 -- Database creation script
+
+create table Orders (
+OrderID int autoincremet,
+PlacementDate datetime,
+CustomerName varchar(200)
+);
+
+create table OrderItems (
+OrderItemID int autoincrement,
+OrderID int,
+ItemID int,
+ItemCount int
+);
+
+create table Items (
+ItemID int autoincrement,
+ItemName varchar(200),
+ItemCost numeric (12,2)
+);
+
+insert into Orders (OrderID, PlacementDate, CustomerName)
+values (1,now(),'Brandon Fox');
+
+
+insert into Orders (OrderID, PlacementDate, CustomerName)
+values (2,now(),'Jack Sparrow');
+
