3bd0c32938c2397257bce5301e216b7491043f4e
Maria Teresa Novillo <mtn2085@MacBook-Air-de-Maria.local>
2018-06-18 18:29:17 -0300

    adding some more data

--- db/create_db.sql
+++ db/create_db.sql
@@ -1,27 +1,33 @@
+-- Database information
+
+user: modusbox
+pass: modusbox
+db:   modusbox_db@localhost
+
+
 -- Database creation script
 
 create table Orders (
-OrderID int autoincremet,
+OrderID int primary key auto_increment,
 PlacementDate datetime,
 CustomerName varchar(200)
 );
 
 create table OrderItems (
-OrderItemID int autoincrement,
+OrderItemID int primary key auto_increment,
 OrderID int,
 ItemID int,
 ItemCount int
 );
 
 create table Items (
-ItemID int autoincrement,
+ItemID int primary key auto_increment,
 ItemName varchar(200),
 ItemCost numeric (12,2)
 );
 
 insert into Orders (OrderID, PlacementDate, CustomerName)
 values (1,now(),'Brandon Fox');
-
 
 insert into Orders (OrderID, PlacementDate, CustomerName)
 values (2,now(),'Jack Sparrow');
