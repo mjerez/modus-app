c55e213bf61c09b4410e8e885589d8a671c1be4a
mjerez <manuel.jerez@gmail.com>
2018-11-07 13:55:21 -0800

    db script

--- db/create_db.sql
+++ db/create_db.sql
@@ -7,6 +7,8 @@ db:   modusbox_db@localhost
 
 -- Database creation script
 
+use mule_db
+
 create table Orders (
 OrderID int primary key auto_increment,
 PlacementDate datetime,
@@ -31,4 +33,29 @@ values (1,now(),'Brandon Fox');
 
 insert into Orders (OrderID, PlacementDate, CustomerName)
 values (2,now(),'Jack Sparrow');
+
+insert into OrderItems (OrderID, OrderItemID, ItemID, ItemCount)
+values (1,1,1,3);
+
+insert into OrderItems (OrderID, OrderItemID, ItemID, ItemCount)
+values (1,2,2,1);
+
+
+insert into OrderItems (OrderID, OrderItemID, ItemID, ItemCount)
+values (2,21,1,3);
+
+insert into OrderItems (OrderID, OrderItemID, ItemID, ItemCount)
+values (2,22,2,1);
+
+insert into Items (ItemID, ItemName, ItemCost)
+values (1,'Mule in Action', 59);
+
+insert into Items (ItemID, ItemName, ItemCost)
+values (2, 'Enterprise Patterns in Action', 49);
+
+insert into Items (ItemID, ItemName, ItemCost)
+values (3, 'Spring in Action',64.5);
+
+insert into Items (ItemID, ItemName, ItemCost)
+values (4, 'Learn to Drive Smart', 9.99);
 
